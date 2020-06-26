38658d921ba408dfa8c65e682ca218a81f085947
ckgard27 <ckgard27@gmail.com>
2020-06-26 09:30:35 -0600

    js setup

---
+++ app.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 6 */
+console.log('connected');
+
+// if('serviceWorker' in navigator){
+//     navigator.serviceWorker.register('sw.js')
+//         .then((reg) => console.log("Service Worker Registered", reg))
+//         .catch((err) => console.log("Service Worker Not Registered", err));
+// }
+
+var app = new Vue({
+    el: '#app',
+    data: {
+
+    },
+    methods: {
+
+    }
+});
