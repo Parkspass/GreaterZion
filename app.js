92a637bed2524b419e3ed1f58b96773655f52159
cade.gardner <d00299297@dmail.dixie.edu>
2020-07-07 12:01:53 -0600

    waiting on API's

--- app.js
+++ app.js
@@ -214,27 +214,59 @@ var app = new Vue({
 		},
         loadStats: function(){
             var vm = this;
+            if (this.page == 'entrances'){
+                axios.get("https://trailwaze.info/zion/vehicleTraffic_request.php?site=zionsouthin").then(response =>{
+                    this.SouthEntranceStat = response.data.zionsouthin.rotate100;
+                    if(this.SouthEntranceStat < 33){
+                        this.southEntranceSvg = "icons/entrance_green.svg";
+                        this.southEntranceSvgStroke = "#749D4C";
+                        this.SouthEntranceBusiness = "Not busy";
+                    }else if(this.SouthEntranceStat < 66){
+                        this.southEntranceSvg = "icons/entrance_yellow.svg";
+                        this.southEntranceSvgStroke = "#FFCD31";
+                        this.SouthEntranceBusiness = "A little busy";
+                    }else{
+                        this.southEntranceSvg = "icons/entrance_pink.svg";
+                        this.southEntranceSvgStroke = "#EF6A6E";
+                        this.SouthEntranceBusiness = "As busy as it gets";
+                    }
+                    this.SouthEntranceStat /= 100;
+                    this.setStop("southEntranceLine", 8, this.SouthEntranceStat);
+                }).catch(error =>{
+                    vm = "Fetch " + error;
+                });
+                axios.get("https://trailwaze.info/zion/vehicleTraffic_request.php?site=zioneastin").then(response =>{
+                    this.EastEntranceStat = response.data.zioneastin.rotate100;
+                    if(this.EastEntranceStat < 33){
+                        this.eastEntranceSvg = "icons/entrance_green.svg";
+                        this.eastEntranceSvgStroke = "#749D4C";
+                        this.EastEntranceBusiness = "Not busy";
+                    }else if(this.EastEntranceStat < 66){
+                        this.eastEntranceSvg = "icons/entrance_yellow.svg";
+                        this.eastEntranceSvgStroke = "#FFCD31";
+                        this.EastEntranceBusiness = "A little busy";
+                    }else{
+                        this.eastEntranceSvg = "icons/entrance_pink.svg";
+                        this.eastEntranceSvgStroke = "#EF6A6E";
+                        this.EastEntranceBusiness = "As busy as it gets";
+                    }
+                    this.EastEntranceStat /= 100;
+                    this.setStop("eastEntranceLine", 8, this.EastEntranceStat);
+                }).catch(error =>{
+                    vm = "Fetch " + error;
+                });
+            }
             axios.get("https://trailwaze.info/zion/request.php").then(response => {
-                //South Entrance: Today
-				vm.SouthEntranceStat = this.getAPIData_safe(response.data, ["ZionSouthEntrance2", "Today", "count"], 0);
-                vm.SouthEntranceStat += this.getAPIData_safe(response.data, ["ZionSouthEntrance3", "Today", "count"], 0);
-                //East Entrance: Today
-                vm.EastEntranceStat = this.getAPIData_safe(response.data, ["ZionEastEntrance1", "Today", "count"], 0);
-                //River Entrance: Today
-                vm.RiverEntranceStat = 0;
-                //Kolob Entrance: Today
-                vm.KolobEntranceStat = 'closed';
-
                 //Visitor Center: Today
                 vm.vcStat = this.getAPIData_safe(response.data, ["ParkingVisitorCenter", "Today", "count"], 0);
                 //Museum: Today
-                vm.museumStat = 0;
+                vm.museumStat = this.getAPIData_safe(response.data, ["ParkingVisitorCenter", "Today", "count"], 0);
                 //RV: Today
-                vm.rvStat = 0;
+                vm.rvStat = this.getAPIData_safe(response.data, ["ParkingVisitorCenter", "Today", "count"], 0);
                 //Springdale: Today
                 vm.springdaleStat = 0;
                 //Kolob Visitor Center
-                vm.kolobVcStat = 0;
+                vm.kolobVcStat = 'closed';
 
                 // ALL TRAILS
                 vm.parusStat = 50;
@@ -254,10 +286,6 @@ var app = new Vue({
                 vm.hiddenCanyonStat = 'closed';
                 vm.observationPointStat = 'closed';
                 vm.narrowsStat = 90; 
-
-                if (this.page == 'entrances'){
-                    this.loadEntrances();
-                }
                 if (this.page == 'parking'){
                     this.loadParking();
                 }
@@ -267,43 +295,6 @@ var app = new Vue({
             }).catch(error => {
                 vm = "Fetch " + error;
             });
-        },
-        loadEntrances: function(){
-            var S = this.SouthEntranceStat / 3000;
-            this.SouthEntranceBusiness = this.loadEntranceBusiness(S)[0];
-            this.southEntranceSvg = this.loadEntranceBusiness(S)[1];
-            this.southEntranceSvgStroke = this.loadEntranceBusiness(S)[2];
- 
-            var E = this.EastEntranceStat / 1000;
-            this.EastEntranceBusiness = this.loadEntranceBusiness(E)[0];
-            this.eastEntranceSvg = this.loadEntranceBusiness(E)[1];
-            this.eastEntranceSvgStroke = this.loadEntranceBusiness(E)[2];
-
-            var R = this.RiverEntranceStat / 100;
-            this.RiverEntranceBusiness = this.loadEntranceBusiness(R)[0];
-            this.riverEntranceSvg = this.loadEntranceBusiness(R)[1];
-            this.riverEntranceSvgStroke = this.loadEntranceBusiness(R)[2];
-
-            var K = this.KolobEntranceStat / 100;
-            this.KolobEntranceBusiness = this.loadEntranceBusiness(K)[0];
-            this.kolobEntranceSvg = this.loadEntranceBusiness(K)[1];
-            this.kolobEntranceSvgStroke = this.loadEntranceBusiness(K)[2];
-
-            this.setStop("southEntranceLine", 8, S);
-            this.setStop("eastEntranceLine", 8, E);
-            this.setStop("riverEntranceLine", 8, R);
-            this.setStop("kolobEntranceLine", 8, K);
-        },
-        loadEntranceBusiness: function(entrance){
-            if (isNaN(entrance) ){
-                return ['Closed', 'icons/entrance_grey.svg', '#B5B5B5'];
-            }else if(entrance < 0.33){
-                return ['Not busy', 'icons/entrance_green.svg', '#749D4C'];
-            }else if(entrance < 0.66){
-                return ['A little busy', 'icons/entrance_yellow.svg', '#FFCD31'];
-            }else{
-                return ['As busy as it gets', 'icons/entrance_pink.svg', '#EF6A6E'];
-            }
         },
         loadParking: function(){
             var VC = this.vcStat / 100;
@@ -350,102 +341,107 @@ var app = new Vue({
         },
         loadTrails: function(){
             var P = this.parusStat/100;
+            var A = this.archeologyStat/100;
+            var LE = this.lowerEmeraldStat/100;
+            var G = this.grottoStat/100;
+            var W = this.weepingRockStat/100;
+            var R = this.riversideStat/100;
+            var WL = this.watchmanStat/100;
+            var SB = this.sandBenchStat/100;
+            var UE = this.upperEmeraldStat/100;
+            var K = this.kayentaStat/100;
+            var CO = this.canyonOverlookStat/100;
+            var TC = this.taylorCreekStat/100;
+            var TiC = this.timberCreekStat/100;
+            var AW = this.angelsLandingWestStat/100;
+            var HC = this.hiddenCanyonStat/100;
+            var OP = this.observationPointStat/100;
+            var N = this.narrowsStat/100;
+            var time = new Date().getHours();
+            if (time < 9 || time > 21 ){
+                P = A = LE = G = W = R = WL = SB = UE = K = CO = TC = TiC = AW = HC = OP = N = 0;
+            }
+
             this.parusBusiness = this.loadTrailsBusiness(P)[0];
             this.parusSvg = this.loadTrailsBusiness(P)[1];
             this.parusSvgStroke = this.loadTrailsBusiness(P)[2];
             this.setStop("parusLine", 8, P);
-
-            var A = this.archeologyStat/100;
+            
             this.archeologyBusiness = this.loadTrailsBusiness(A)[0];
             this.archeologySvg = this.loadTrailsBusiness(A)[1];
             this.archeologySvgStroke = this.loadTrailsBusiness(A)[2];
             this.setStop("archeologyLine", 8, A);
 
-            var LE = this.lowerEmeraldStat/100;
             this.lowerEmeraldBusiness = this.loadTrailsBusiness(LE)[0];
             this.lowerEmeraldSvg = this.loadTrailsBusiness(LE)[1];
             this.lowerEmeraldSvgStroke = this.loadTrailsBusiness(LE)[2];
             this.setStop("lowerEmeraldLine", 8, LE);
 
-            var G = this.grottoStat/100;
             this.grottoBusiness = this.loadTrailsBusiness(G)[0];
             this.grottoSvg = this.loadTrailsBusiness(G)[1];
             this.grottoSvgStroke = this.loadTrailsBusiness(G)[2];
             this.setStop("grottoLine", 8, G);
 
-            var W = this.weepingRockStat/100;
             this.weepingRockBusiness = this.loadTrailsBusiness(W)[0];
             this.weepingRockSvg = this.loadTrailsBusiness(W)[1];
             this.weepingRockSvgStroke = this.loadTrailsBusiness(W)[2];
             this.setStop("weepingRockLine", 8, W);
 
-            var R = this.riversideStat/100;
             this.riversideBusiness = this.loadTrailsBusiness(R)[0];
             this.riversideSvg = this.loadTrailsBusiness(R)[1];
             this.riversideSvgStroke = this.loadTrailsBusiness(R)[2];
             this.setStop("riversideLine", 8, R);
 
-            var WL = this.watchmanStat/100;
             this.watchmanBusiness = this.loadTrailsBusiness(WL)[0];
             this.watchmanSvg = this.loadTrailsBusiness(WL)[1];
             this.watchmanSvgStroke = this.loadTrailsBusiness(WL)[2];
             this.setStop("watchmanLine", 8, WL);
 
-            var SB = this.sandBenchStat/100;
             this.sandBenchBusiness = this.loadTrailsBusiness(SB)[0];
             this.sandBenchSvg = this.loadTrailsBusiness(SB)[1];
             this.sandBenchSvgStroke = this.loadTrailsBusiness(SB)[2];
             this.setStop("sandBenchLine", 8, SB);
 
-            var UE = this.upperEmeraldStat/100;
             this.upperEmeraldBusiness = this.loadTrailsBusiness(UE)[0];
             this.upperEmeraldSvg = this.loadTrailsBusiness(UE)[1];
             this.upperEmeraldSvgStroke = this.loadTrailsBusiness(UE)[2];
             this.setStop("upperEmeraldLine", 8, UE);
 
-            var K = this.kayentaStat/100;
             this.kayentaBusiness = this.loadTrailsBusiness(K)[0];
             this.kayentaSvg = this.loadTrailsBusiness(K)[1];
             this.kayentaSvgStroke = this.loadTrailsBusiness(K)[2];
             this.setStop("kayentaLine", 8, K);
 
-            var CO = this.canyonOverlookStat/100;
             this.canyonOverlookBusiness = this.loadTrailsBusiness(CO)[0];
             this.canyonOverlookSvg = this.loadTrailsBusiness(CO)[1];
             this.canyonOverlookSvgStroke = this.loadTrailsBusiness(CO)[2];
             this.setStop("canyonOverlookLine", 8, CO);
 
-            var TC = this.taylorCreekStat/100;
             this.taylorCreekBusiness = this.loadTrailsBusiness(TC)[0];
             this.taylorCreekSvg = this.loadTrailsBusiness(TC)[1];
             this.taylorCreekSvgStroke = this.loadTrailsBusiness(TC)[2];
             this.setStop("taylorCreekLine", 8, TC);
-
-            var TiC = this.timberCreekStat/100;
+            
             this.timberCreekBusiness = this.loadTrailsBusiness(TiC)[0];
             this.timberCreekSvg = this.loadTrailsBusiness(TiC)[1];
             this.timberCreekSvgStroke = this.loadTrailsBusiness(TiC)[2];
             this.setStop("timberCreekLine", 8, TiC);
 
-            var AW = this.angelsLandingWestStat/100;
             this.angelsLandingWestBusiness = this.loadTrailsBusiness(AW)[0];
             this.angelsLandingWestSvg = this.loadTrailsBusiness(AW)[1];
             this.angelsLandingWestSvgStroke = this.loadTrailsBusiness(AW)[2];
             this.setStop("angelsLandingWestLine", 8, AW);
 
-            var HC = this.hiddenCanyonStat/100;
             this.hiddenCanyonBusiness = this.loadTrailsBusiness(HC)[0];
             this.hiddenCanyonSvg = this.loadTrailsBusiness(HC)[1];
             this.hiddenCanyonSvgStroke = this.loadTrailsBusiness(HC)[2];
             this.setStop("hiddenCanyonLine", 8, HC);
 
-            var OP = this.observationPointStat/100;
             this.observationPointBusiness = this.loadTrailsBusiness(OP)[0];
             this.observationPointSvg = this.loadTrailsBusiness(OP)[1];
             this.observationPointSvgStroke = this.loadTrailsBusiness(OP)[2];
             this.setStop("observationPointLine", 8, OP);
 
-            var N = this.narrowsStat/100;
             this.narrowsBusiness = this.loadTrailsBusiness(N)[0];
             this.narrowsSvg = this.loadTrailsBusiness(N)[1];
             this.narrowsSvgStroke = this.loadTrailsBusiness(N)[2];
